cc7e380f75f3d93dd75845aca101223d855ccb60
Sumit Sahrawat <sumitsahrawat@outlook.com>
2018-02-13 16:59:22 +0530

    Remove reference to missing ~/.gtkrc-2.0

--- variables.zsh
+++ variables.zsh
@@ -20,6 +20,5 @@ elif (( $+commands[vi] )); then
 fi
 
 export QT_QPA_PLATFORMTHEME="qt5ct"
-export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 # fix "xdg-open fork-bomb" export your preferred browser from here
 export BROWSER=/usr/bin/qutebrowser
