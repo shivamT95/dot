af5bf8fce43965e569058dbff6a91901fcf2d6b2
Sumit Sahrawat <sumitsahrawat@outlook.com>
2018-03-04 14:01:59 +0530

    Clean up variables.zsh

--- zsh/.zsh/variables.zsh
+++ zsh/.zsh/variables.zsh
@@ -8,21 +8,18 @@ export PATH
 if (( $+commands[emacs] )); then
     export EDITOR='emacsclient -t'
     export ALTERNATE_EDITOR=""
-    export SUDO_EDITOR=$EDITOR
     export VISUAL='emacsclient -c'
 elif (( $+commands[nvim] )); then
     export EDITOR=nvim
-    export SUDO_EDITOR=nvim
 elif (( $+commands[vim] )); then
     export EDITOR=vim
-    export SUDO_EDITOR=vim
     if (( $+commands[gvim] )); then
         export VISUAL=gvim
     fi
 elif (( $+commands[vi] )); then
     export EDITOR=vi
-    export SUDO_EDITOR=vi
 fi
+export SUDO_EDITOR=$EDITOR
 
 if (( $+commands[qutebrowser] )); then
     export BROWSER=qutebrowser
